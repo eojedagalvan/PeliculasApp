1e2196257ee0a0c781cc20e69da346cf52f2d708
Eduardo Ojeda <ojedaeduardo2001@gmail.com>
2021-12-18 21:30:07 -0600

    Mostrar el poster de la película

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
 import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import MoviePoster from "../components/MoviePoster";
 import { useMovies } from "../hooks/useMovies";
 
 const HomeScreen = () => {
   const { peliculasEnCine, isLoading } = useMovies();
+  const { top } = useSafeAreaInsets();
 
   if (isLoading) {
     return (
@@ -19,8 +22,12 @@ const HomeScreen = () => {
     );
   }
   return (
-    <View>
-      <Text>Hola</Text>
+    <View
+      style={{
+        marginTop: top + 20,
+      }}
+    >
+      <MoviePoster movie={peliculasEnCine[0]} />
     </View>
   );
 };
