0363770de612cfc1c8f7a2a54a4c00407ae68a68
Eduardo Ojeda <ojedaeduardo2001@gmail.com>
2022-01-04 17:15:05 -0600

    Poner en un context los colores de las imgs

--- App.tsx
+++ App.tsx
@@ -4,12 +4,19 @@ import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { Navigation } from "./src/navigation/Navigation";
 import FadeScreen from "./src/screens/FadeScreen";
+import { GradientProvider } from "./src/context/GradientContext";
+
+const AppState = ({ children }: any) => {
+  return <GradientProvider>{children}</GradientProvider>;
+};
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Navigation />
-      {/* <FadeScreen /> */}
+      <AppState>
+        <Navigation />
+        {/* <FadeScreen /> */}
+      </AppState>
     </NavigationContainer>
   );
 };
